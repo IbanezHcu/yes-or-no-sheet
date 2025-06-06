901cd1870a7efeab90bace4478cabd06ef054e65
Ibanez Hcu <ibanezhcu@gmail.com>
2025-06-06 16:19:22 +0700

    Fix sheet connection using open_by_key

--- yes_or_no_gsheet.py
+++ yes_or_no_gsheet.py
@@ -1,23 +1,25 @@
 import streamlit as st
 import gspread
-import pandas as pd
 from google.oauth2.service_account import Credentials
 
 @st.cache_resource
 def connect_sheet():
     scope = [
-        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
+
     creds = Credentials.from_service_account_info(
         st.secrets["gsheets"],
         scopes=scope
     )
+
     client = gspread.authorize(creds)
-    sheet = client.open("yes-or-no-data").sheet1
+
+    # เปิดโดยใช้ Spreadsheet ID
+    sheet = client.open_by_key("1PdfJSe1YZRYfIseGgKngjqzv_CrwpTddLcLoaKOLw3A").sheet1
     return sheet
 
+# ใช้งาน sheet
 sheet = connect_sheet()
-
-# ตัวอย่าง: แสดงข้อมูลทั้งหมด
 st.write(sheet.get_all_values())
